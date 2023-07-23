71c24fcb901f148529921a5a321c756bcab756b0
rain <xiaoyu19910407@163.com>
2023-07-23 21:16:11 +0800

    C1客户端dev01修改Test01.java

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -11,6 +11,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01...");
         System.out.println("test02...");
-        System.out.println("test05...");
+        System.out.println("test10...");
     }
 }
