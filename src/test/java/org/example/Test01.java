726071bb5f96f4719e1ee228fda53eea88916d89
rain <xiaoyu19910407@163.com>
2023-07-23 18:05:55 +0800

    添加Test01.java

---
+++ src/test/java/org/example/Test01.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @Author: Rain
+ * @Date: 2023/7/23 0023 17:59:48
+ * @Version: 1.0
+ * @Describe: org.example
+ * @ProjectName: mygit
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+    }
+}
