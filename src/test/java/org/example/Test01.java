e2bf633d54394cecf46360be61b74a44c53d1137
rain <xiaoyu19910407@163.com>
2023-07-23 18:53:29 +0800

    Revert "添加Test01.java"
    This reverts commit 8003e79f3feaa9710a38c80e107df69f4127b5ba.

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -10,7 +10,5 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01...");
-        System.out.println("test02...");
-        System.out.println("test03...");
     }
 }
